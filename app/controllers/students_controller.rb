0abf9013685f78fd26b0f3945d035e5817de221f
Vince Andrew Pimentel <contact@vincep.me>
2019-01-29 04:38:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,8 +11,7 @@ class StudentsController < ApplicationController
   def new
     Student.create(first_name: params[:first_name], last_name: params[:last_name])
 
-    redirect_to students_path
-    
+    redirect_to students_path    
   end
 
 end
